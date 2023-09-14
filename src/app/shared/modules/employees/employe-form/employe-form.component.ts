700672256bbfff38e2d45468c2a7e5f9ad37343e
Mangesh047 <dholem39@gmail.com>
2023-09-14 23:36:27 +0530

    reactive-form added employee

--- src/app/shared/modules/employees/employe-form/employe-form.component.ts
+++ src/app/shared/modules/employees/employe-form/employe-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckboxControlValueAccessor, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-employe-form',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeFormComponent implements OnInit {
 
-  constructor() { }
+  employeForm!: FormGroup
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.employeForm = this._fb.group({
+      fname: [null, Validators.required],
+      lname: [null, Validators.required],
+      contact: [null, Validators.required],
+      email: [null, Validators.required],
+      currentAddress: [null, Validators.required],
+      permentAddress: [null, Validators.required],
+      city: [null, Validators.required],
+      state: [null, Validators.required],
+      pincode: [null, Validators.required],
+      bankName: [null, Validators.required],
+      ifscCode: [null, Validators.required],
+      salary: [null, Validators.required],
+    })
+  }
+  flag: boolean = false
+  sameAddres() {
+
+    this.flag = !this.flag
+
+    if (this.flag) {
+      this.employeForm.controls['permentAddress'].patchValue(this.employeForm.controls['currentAddress'].value)
+    } else {
+      this.employeForm.controls['permentAddress'].reset()
+    }
+
   }
 
+  onEmployeAdd() {
+    if (this.employeForm.valid) {
+      console.log(this.employeForm.value);
+    }
+
+  }
 }
--- src/app/shared/modules/employees/employees.module.ts
+++ src/app/shared/modules/employees/employees.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { EmployeTableComponent } from './employe-table/employe-table.component';
 import { EmployeFormComponent } from './employe-form/employe-form.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +14,8 @@ import { MaterialModule } from '../material/material.module';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class EmployeesModule { }
