cedfb998d184672af01a3da3a939640e4c428af9
vs2105 <117720664+vs2105@users.noreply.github.com>
2023-09-15 15:29:59 +0530

    attendance-form

--- src/app/shared/modules/attendance/attendance.module.ts
+++ src/app/shared/modules/attendance/attendance.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AttendanceTableComponent } from './attendace-table/attendance-table/attendance-table.component';
 import { AttendanceFormComponent } from './attendance-form/attendance-form.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -11,7 +12,8 @@ import { AttendanceFormComponent } from './attendance-form/attendance-form.compo
     AttendanceFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class AttendanceModule { }
